b22f8cc81d1fd8f3cc760e2899f6dfee02b82960
TurobSulaymonov <turobsulaymonov117@gmail.com>
2024-10-17 22:23:42 +0900

    feat: develop contact ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,29 @@
+import Hero from "@/components/Hero";
+const TITLE_Class = "text-2xl font-bold text-gray-800 my-2"
 export default function AboutPage() {
-  return <p>나에대해!</p>;
+  return (
+    <>
+    <Hero/>
+    <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+      <h2 className={TITLE_Class}>
+        Who am I?
+      </h2>
+      <p>
+        개발을 사랑하는 FullStack 개발자 <br />
+        사람과 디자인을 담는 웹앱을 만들고 있음
+      </p>
+      <h2 className={TITLE_Class}>Career</h2>
+      <p>경복대학교(-Now) <br />
+       UzbekTelecom(-2018)
+      </p>
+      <h2 className={TITLE_Class}>Skills</h2>
+      <p>
+        React, Node Nestjs <br />
+        Git, Clean Code <br />
+        Vs Code, MongoDb, FireBase
+      </p>
+    </section>
+    </>
+  );
+
 }
